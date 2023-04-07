27babbcf06f95db6d8fbfdec9c12d57a327957ea
Pavlo Daniv <paulsegfault@gmail.com>
2023-04-07 20:06:54 +0300

    Update build-java image

--- bin/build_images.sh
+++ bin/build_images.sh
@@ -1,11 +1,8 @@
 #!/usr/bin/env bash
 
 BUILD_OPTS="--no-cache"
-BUILD_OPTS="${BUILD_OPTS} --build-arg AWS_ACCESS_KEY_ID"
-BUILD_OPTS="${BUILD_OPTS} --build-arg AWS_SECRET_ACCESS_KEY"
 
 GITHUB_CONTAINER_REGISTRY_URL="ghcr.io/pauldaniv/yellow-taxi-scripts"
-ECR_CONTAINER_REGISTRY_URL="375158168967.dkr.ecr.us-east-2.amazonaws.com"
 
 BASE_IMAGES=( "base" "terraform" )
 
@@ -23,27 +20,7 @@ function build_and_push_top_level_image() {
   build_and_push_image "$image" "$GITHUB_CONTAINER_REGISTRY_URL"
 }
 
-function build_and_push_second_level_image() {
-  build_and_push_image "$image" "ECR_CONTAINER_REGISTRY_URL"
-}
-
 cd "$(cd "$(dirname "$0")/.."; pwd)"
-
-#echo "Building base images..."
-#for image in "${BASE_IMAGES[@]}"; do
-#  build_and_push_top_level_image $image
-#done
-
-#echo "Logging to ECR..."
-#aws ecr get-login-password --region us-east-2\
-# | docker login --username AWS --password-stdin 375158168967.dkr.ecr.us-east-2.amazonaws.com
-
-#if [[ $? != 0 ]]; then
-#  echo "Unable to login to ECR"
-#  exit 1
-#else
-#  echo "Login to ECR succeeded!"
-#fi
 
 for image in $(ls src); do
   build_and_push_top_level_image $image
