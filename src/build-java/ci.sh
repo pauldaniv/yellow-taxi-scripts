ddb2c08b07bb63382c0ed014d87b7ed2b7edabff
Pavlo Daniv <paulsegfault@gmail.com>
2023-04-07 22:21:02 +0300

    Update build-java image

--- src/build-java/ci.sh
+++ src/build-java/ci.sh
@@ -1,9 +1,48 @@
 #!/usr/bin/env bash
 
-chmod +x gradlew
-./gradlew test
+ACTION=$1
 
 ECR_CONTAINER_REGISTRY_URL="375158168967.dkr.ecr.us-east-2.amazonaws.com"
-echo "building..."
-ls
-./gradlew bootBuildImage --imageName "$ECR_CONTAINER_REGISTRY_URL/`basename`:latest"
+
+if [[ -z "$ACTION" ]]; then
+  echo "No command specified, available commands [build, test, deploy]"
+  exit 1
+fi
+
+chmod +x gradlew
+
+function runBuild() {
+  echo "Building..."
+  ./gradlew build
+}
+
+function runTest() {
+  echo "Running tests..."
+  ./gradlew test
+}
+
+function publishArtifacts() {
+  echo "Publishing artifacts..."
+  ./gradlew deploy --imageName "$ECR_CONTAINER_REGISTRY_URL/`basename`:latest"
+}
+
+function deploy() {
+  echo "Deploying..."
+  local image_name="$ECR_CONTAINER_REGISTRY_URL/`basename`:latest"
+  ./gradlew bootBuildImage --imageName "$ECR_CONTAINER_REGISTRY_URL/`basename`:latest"
+  docker push $image_name
+}
+
+
+if [[ "$ACTION" == "build" ]]; the
+  runBuild
+elif [[ "$ACTION" == "test"]]; then
+  runTest
+elif [[ "$ACTION" == "publish"]]; then
+    publishArtifacts
+elif [[ "$ACTION" == "deploy"]]; then
+  deploy
+else
+  echo "Unknown command $ACTION"
+  exit 1
+fi
