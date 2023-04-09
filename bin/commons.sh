9081774558b3525956e9d14382b4e67b4d028948
Pavlo Daniv <paulsegfault@gmail.com>
2023-04-09 20:12:49 +0300

    Fix image build tags. Add separate image version support

--- bin/commons.sh
+++ bin/commons.sh
@@ -4,5 +4,6 @@ IMAGE_NAME=$1
 GITHUB_CONTAINER_REGISTRY_URL="ghcr.io/pauldaniv/yellow-taxi-scripts"
 
 function get_version() {
-  cat version.txt
+  local path_to_version_file=$1
+  [[ -f src/${path_to_version_file}/version.txt ]] && cat src/${path_to_version_file}/version.txt || cat version.txt
 }
